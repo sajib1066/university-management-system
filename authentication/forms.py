a195a678805de7bc230240723c69dddc10a4bd34
sajib1066 <sajib1066@gmail.com>
2019-12-20 22:34:42 +0600

    update registration system

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
 
 class RegistrationForm(forms.Form):
     username = forms.CharField(widget=(forms.TextInput(attrs={
@@ -16,6 +19,38 @@ class RegistrationForm(forms.Form):
     confirm_email = forms.EmailField(widget=(forms.TextInput(attrs={
         'class': 'form-control'
     })))
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username=username)
+        if r.count():
+            raise ValidationError('Username already exists!')
+        return username
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password and confirm_password and password != confirm_password:
+            raise ValidationError('Password don`t match!')
+        return confirm_password
+
+    def clean_confirm_email(self):
+        email = self.cleaned_data['email'].lower()
+        confirm_email = self.cleaned_data['confirm_email'].lower()
+        if email and confirm_email and email != confirm_email:
+            raise ValidationError('Email don`t match!')
+        r = User.objects.filter(email=confirm_email)
+        if r.count():
+            raise ValidationError('Email already exists!')
+        return confirm_email
+
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['confirm_password'],
+            self.cleaned_data['confirm_email']
+        )
+        return user
 
 
 class LoginForm(forms.Form):
--- authentication/views.py
+++ authentication/views.py
@@ -1,32 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 from .forms import RegistrationForm, LoginForm
 
 def user_registration(request):
     if request.user.is_authenticated:
         return redirect('home')
-    forms = RegistrationForm()
+    # forms = RegistrationForm()
     if request.method == 'POST':
         forms = RegistrationForm(request.POST)
         if forms.is_valid():
-            username = forms.cleaned_data['username']
-            password = forms.cleaned_data['password']
-            confirm_password = forms.cleaned_data['confirm_password']
-            email = forms.cleaned_data['email']
-            confirm_email = forms.cleaned_data['confirm_email']
-            if password == confirm_password and email == confirm_email:
-                User.objects.create_user(
-                    username=username,
-                    password=password,
-                    email = email
-                )
-                user = authenticate(username=username, password=password)
-                if user:
-                    login(request, user)
-                    return redirect('home')
-            else:
-                print('Data not matched!')
+            forms.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('home')
+    else:
+        forms = RegistrationForm()
     context = {
         'forms': forms
     }
