ca97cfdecf9c8389b1de538f46495f42235ae9a0
sajib1066 <sajib1066@gmail.com>
2019-12-22 20:48:33 +0600

    create faculty model

--- academic/models.py
+++ academic/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Faculty(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    established = models.DateField()
+    photo = models.ImageField(upload_to='faculty/')
+    about = models.TextField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
