d3d99e34fecd922716f466caf7a4007a9fa5e6aa
sajib1066 <sajib1066@gmail.com>
2019-12-24 21:38:03 +0600

    create add faculty form

---
+++ academic/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Faculty
+
+class FacultyForm(forms.ModelForm):
+    class Meta:
+        model = Faculty
+        fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Name"}),
+            'established': forms.SelectDateWidget()
+        }
--- academic/views.py
+++ academic/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .forms import FacultyForm
 
 def add_faculty(request):
-    return render(request, 'faculty/add-faculty.html')
+    forms = FacultyForm()
+    context = {
+        'forms': forms
+    }
+    return render(request, 'faculty/add-faculty.html', context)
