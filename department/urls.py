695872921310601095dde814692d087e72b75185
sajib1066 <sajib1066@gmail.com>
2020-01-03 20:42:37 +0600

    create department list

--- department/urls.py
+++ department/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-department/', views.add_department, name='add-department')
+    path('add-department/', views.add_department, name='add-department'),
+    path('department-list/', views.department_list, name='department-list')
 ]
--- department/views.py
+++ department/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import DepartmentForm
+from .models import Department
 
 def add_department(request):
     forms = DepartmentForm()
@@ -7,8 +8,15 @@ def add_department(request):
         forms = DepartmentForm(request.POST)
         if forms.is_valid():
             forms.save()
-            return redirect('home')
+            return redirect('department-list')
     context = {
         'form': forms
     }
     return render(request, 'department/add-department.html', context)
+
+def department_list(request):
+    department = Department.objects.all()
+    context = {
+        'department': department
+    }
+    return render(request, 'department/department-list.html', context)
