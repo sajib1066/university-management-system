6ddcc4d374349dddba44c5a986bbbd485de693ff
sajib1066 <sajib1066@gmail.com>
2020-01-03 21:00:49 +0600

    create professor app

---
+++ professor/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProfessorConfig(AppConfig):
+    name = 'professor'
--- ums_config/settings.py
+++ ums_config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'authentication',
     'faculty',
     'department',
+    'professor',
 ]
 
 MIDDLEWARE = [
