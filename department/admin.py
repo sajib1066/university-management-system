5f41ea14fa21ac206d3acf603a3bd53bb886a78c
sajib1066 <sajib1066@gmail.com>
2020-01-03 19:54:48 +0600

    create department model

--- department/admin.py
+++ department/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Department
 
-# Register your models here.
+admin.site.register(Department)
--- department/models.py
+++ department/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from faculty.models import Faculty
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    name_of_head = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=11, unique=True)
+    status_choice = (
+        ('active', 'Active'),
+        ('disabled', 'Disabled'),
+        ('paused', 'Paused')
+    )
+    status = models.CharField(choices=status_choice, max_length=20)
+    opening_date = models.DateField()
+
+    def __str__(self):
+        return self.name
