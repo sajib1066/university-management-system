f3d6ff1d1cab936a09cf1b1031d157a5df0c7daf
sajib1066 <sajib1066@gmail.com>
2020-01-04 22:00:53 +0600

    create address models

--- address/admin.py
+++ address/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Country, State, City
 
-# Register your models here.
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(City)
--- address/models.py
+++ address/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    country_name = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.country_name
+
+class State(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    state_name = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.state_name
+
+class City(models.Model):
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    city_name = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.city_name
