c312fe85321155d6752e9b6583eebd4beb3fe8c1
sajib1066 <sajib1066@gmail.com>
2020-01-04 19:15:47 +0600

    update department model

--- department/models.py
+++ department/models.py
@@ -14,6 +14,7 @@ class Department(models.Model):
     )
     status = models.CharField(choices=status_choice, max_length=20)
     opening_date = models.DateField()
+    is_delete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
