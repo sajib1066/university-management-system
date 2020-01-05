728aaede43903f056b2ac9e793482191e7216fbd
sajib1066 <sajib1066@gmail.com>
2020-01-05 19:11:54 +0600

    create professor models

--- professor/admin.py
+++ professor/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Designation, Professor
 
-# Register your models here.
+admin.site.register(Designation)
+admin.site.register(Professor)
--- professor/models.py
+++ professor/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from department.models import Department
+from address.models import Country, State, City
 
-# Create your models here.
+class Designation(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class Professor(models.Model):
+    name = models.CharField(max_length=120)
+    phone = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(max_length=120, unique=True)
+    date_of_birth = models.DateField()
+    address = models.TextField()
+    postcode = models.IntegerField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    designation = models.ForeignKey(Designation, on_delete=models.CASCADE)
+    about = models.TextField()
+    gender_choice = (
+        ('male', 'Male'),
+        ('female', 'Female')
+    )
+    gender = models.CharField(choices=gender_choice, max_length=6)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    website = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
